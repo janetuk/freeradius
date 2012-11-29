66c7f073532176ca6607cfa8fbf8f72b87d34058
Sam Hartman <hartmans@debian.org>
2012-11-29 14:30:07 -0500

    realms_realm_add
    Expose an interface for adding a realm.

--- src/include/realms.h
+++ src/include/realms.h
@@ -153,6 +153,7 @@ REALM *realm_find(const char *name); /* name is from a packet */
 REALM *realm_find2(const char *name); /* ... with name taken from realm_find */
   int realms_home_server_add(home_server *home, CONF_SECTION *cs, int dual);
   int realms_pool_add(home_pool_t *pool, CONF_SECTION *cs);
+  int realms_realm_add( REALM *r, CONF_SECTION *cs);
 
 
 void home_server_update_request(home_server *home, REQUEST *request);
